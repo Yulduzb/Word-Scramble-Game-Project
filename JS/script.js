7d4fa238acde35009551ce45c5e0a66bad29a527
Yulduz Buranbaeva <130300539+Yulduzb@users.noreply.github.com>
2023-08-07 20:37:56 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,74 @@
+const wordText=document.querySelector(".word"),
+hintText=document.querySelector(".hint span"),
+inputField=document.querySelector("input"),
+timeText=document.querySelector(".time b"),
+refreshBnt=document.querySelector(".refresh-word"),
+checkBtn=document.querySelector(".check-word");
+
+
+
+let correctWord,timer;
+
+
+
+
+const initTimer = maxTime => {
+    clearInterval(timer);
+     timer = setInterval(() => {
+        if (maxTime > 0) {
+            maxTime--;
+            return timeText.innerText=maxTime;
+           
+        } else {
+            clearInterval(timer);
+            alert(`Maalesef, zamanınız doldu. Doğru kelime: ${correctWord}`);
+            initGame();
+        }
+    }, 1000);
+};
+
+
+const initGame=()=>{
+    initTimer(30)
+    let randomObj=kelimeler[Math.floor(Math.random()*kelimeler.length)];          
+    let wordArray=randomObj.kelime.split("");
+    for(let i=wordArray.length-1;i>0;i--){
+        let j=Math.floor(Math.random() * (i+1));
+        [wordArray[i],wordArray[j]]=[wordArray[j],wordArray[i]];
+    }
+   wordText.innerText=wordArray.join("");
+   if (wordArray.length > 10) {
+    wordText.style.fontSize = "20px";
+   }
+   hintText.innerText=randomObj.ipucu;
+   correctWord=randomObj.kelime.toLowerCase();
+   inputField.value="";
+   inputField.setAttribute("maxlength",correctWord.length);
+}
+
+
+
+initGame();
+
+const checkWord=()=>{
+    let userWord=inputField.value.toLocaleLowerCase();
+    if(!userWord) return alert(`Lütfen bir kelime girin!`);
+    
+
+    if(userWord!==correctWord) return  alert (`Maalesef, ${userWord} yanlış, Tekrar deneyin!`);
+   
+    
+    alert(`Evet, ${userWord}  doğru! Tebrikler!`);
+    initGame();
+}
+
+
+
+inputField.addEventListener("keyup", function(event) {
+  if (event.key === "Enter") {
+    checkWord();
+  }
+});
+
+refreshBnt.addEventListener("click",initGame);
+checkBtn.addEventListener("click",checkWord);
---
+++ README.md
@@ -0,0 +1,12 @@
+ <h1>Word-Scramble-Game-Project</h1>
+
+<p>This project includes a Word Shuffle Game created using JavaScript, HTML and CSS. The game shows the user a jumbled word and waits for the user to decipher it correctly. If the correct answer is given, the user is shown a congratulatory message and the game continues with a new word.</p>
+
+<h2>Technologies Used</h2>
+<h3>The website was developed using the following technologies:</h3>
+
+HTML
+CSS
+Javascript
+
+![](/Image/WordGame.gif)
